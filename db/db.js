3f2c9bd240ae455c931636627f052bcb462c1fff
SexyMan <devnoor00@gmail.com>
2021-04-25 01:30:35 +0300

    finished

--- db/db.js
+++ db/db.js
@@ -1,12 +1,7 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  port: process.env.PORT,
-});
+const pool = new Pool();
+pool.connect(process.env.DATABASE_URL);
 
 const query = (text, params) => pool.query(text, params);
 module.exports = { query };
